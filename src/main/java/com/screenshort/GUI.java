8e922ff174ebceb21dc4bd0c2c8d29fdbb2e7ec1
Toan.Win11 <t15n.ng7y7n@gmail.com>
2025-03-28 16:13:57 +0700

    update

--- src/main/java/com/screenshort/GUI.java
+++ src/main/java/com/screenshort/GUI.java
@@ -3,10 +3,13 @@ package com.screenshort;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
 import burp.api.montoya.MontoyaApi;
 import burp.api.montoya.core.ToolType;
+import burp.api.montoya.http.message.HttpRequestResponse;
 import burp.api.montoya.ui.contextmenu.ContextMenuEvent;
 import burp.api.montoya.ui.contextmenu.ContextMenuItemsProvider;
 
@@ -22,26 +25,48 @@ public class GUI implements ContextMenuItemsProvider {
     @Override
     public List<Component> provideMenuItems(ContextMenuEvent event) {
         List<Component> menuItems = new ArrayList<>();
-        
+
+        // Tạo JMenu thay vì JMenuItem để có thể chứa các mục con
+        JMenu screenshotMenu = new JMenu("Screenshot");
+
         JMenuItem normalMenuItem = new JMenuItem("Normal");
         JMenuItem fullMenuItem = new JMenuItem("Full");
         JMenuItem fullEditedMenuItem = new JMenuItem("Full - Edited Request");
         JMenuItem fullOriginalMenuItem = new JMenuItem("Full - Original Request");
-        
-        
+
         normalMenuItem.addActionListener(e -> menuHandler.handleNormalScreenshot());
         fullMenuItem.addActionListener(e -> menuHandler.handleFullScreenshot());
         fullEditedMenuItem.addActionListener(e -> menuHandler.handleEditedScreenshot());
         fullOriginalMenuItem.addActionListener(e -> menuHandler.handleOriginalScreenshot());
-        
-        menuItems.add(normalMenuItem);
-        menuItems.add(fullMenuItem);
-        
+
+        screenshotMenu.add(normalMenuItem);
+        screenshotMenu.add(fullMenuItem);
+
         if (event.isFromTool(ToolType.PROXY)) {
-            menuItems.add(fullEditedMenuItem);
-            menuItems.add(fullOriginalMenuItem);
+            screenshotMenu.add(fullEditedMenuItem);
+            screenshotMenu.add(fullOriginalMenuItem);
         }
+
+        menuItems.add(screenshotMenu);
+
+        // PCopy
+        JMenu pCopy = new JMenu("PCopy");
+        JMenuItem pCopyhasBody = new JMenuItem("PCopy has body");
+        JMenuItem pCopynoBody = new JMenuItem("PCopy no body");
+        // Lấy HttpRequestResponse từ sự kiện
+        HttpRequestResponse requestResponse = event.messageEditorRequestResponse().isPresent()
+                ? event.messageEditorRequestResponse().get().requestResponse()
+                : event.selectedRequestResponses().get(0);
+
+        pCopyhasBody.addActionListener(e -> menuHandler.handleCopyToExcel(requestResponse));
+        pCopynoBody.addActionListener(e -> menuHandler.handleCopyToExcelNoBody(requestResponse));
+
+        pCopy.add(pCopyhasBody);
+        pCopy.add(pCopynoBody);
         
+        menuItems.add(pCopy);
+
         return menuItems;
     }
+
 }
--- src/main/java/com/screenshort/MenuActionHandler.java
+++ src/main/java/com/screenshort/MenuActionHandler.java
@@ -1,7 +1,20 @@
 package com.screenshort;
 
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.nio.charset.StandardCharsets;
+
 import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.core.ByteArray;
+import burp.api.montoya.http.HttpService;
+import burp.api.montoya.http.message.Cookie;
+import burp.api.montoya.http.message.HttpHeader;
+import burp.api.montoya.http.message.HttpRequestResponse;
+import burp.api.montoya.http.message.params.HttpParameterType;
+import burp.api.montoya.http.message.params.ParsedHttpParameter;
+
+import java.util.List;
 
 public class MenuActionHandler {
     private final MontoyaApi api;
@@ -26,7 +39,7 @@ public class MenuActionHandler {
             Component resComp = ComponentFinder.getComponentByName(targetComponent, "rrvResponsePane");
             Component syntaxTextAreaReq = ComponentFinder.getComponentByName(reqComp, "syntaxTextArea");
             Component syntaxTextAreaRes = ComponentFinder.getComponentByName(resComp, "syntaxTextArea");
-            
+
             action.takeScreenshotAndGetBufferImage(syntaxTextAreaReq);
             action.takeScreenshotAndGetBufferImage(syntaxTextAreaRes);
             action.takeScreenshot2();
@@ -49,24 +62,222 @@ public class MenuActionHandler {
             Component targetComponent = ComponentFinder.findComponentUnderMouse("rrvSplitViewerSplitPane", frame);
             Component reqComp = ComponentFinder.getComponentByName(targetComponent, "rrvRequestsPane");
             Component resComp = ComponentFinder.getComponentByName(targetComponent, "rrvResponsePane");
-            
-            java.util.List<Component> reqComponents = ComponentFinder.findAllComponentsByName(reqComp, "syntaxTextArea");
-            java.util.List<Component> resComponents = ComponentFinder.findAllComponentsByName(resComp, "syntaxTextArea");
-            
+
+            java.util.List<Component> reqComponents = ComponentFinder.findAllComponentsByName(reqComp,
+                    "syntaxTextArea");
+            java.util.List<Component> resComponents = ComponentFinder.findAllComponentsByName(resComp,
+                    "syntaxTextArea");
+
             if (reqComponents.size() <= index || resComponents.size() <= index) {
                 api.logging().logToOutput("No syntaxTextArea found");
                 action.clearImages();
                 return;
             }
-            
+
             Component syntaxTextAreaReq = reqComponents.get(index);
             Component syntaxTextAreaRes = resComponents.get(index);
-            
+
             action.takeScreenshotAndGetBufferImage(syntaxTextAreaReq);
             action.takeScreenshotAndGetBufferImage(syntaxTextAreaRes);
             action.takeScreenshot2();
         } catch (Exception ex) {
             api.logging().logToError("Error: " + ex.getMessage());
         }
+    }
+
+    public void handleCopyToExcel(HttpRequestResponse requestResponse) {
+        ExcelFormatter excelFormatter = new ExcelFormatter();
+
+        // -----------------------------------------------
+        // Method
+        String method = requestResponse.request().method();
+        // Host
+        HttpService httpService = requestResponse.httpService();
+        String hostService = httpService.host();
+        int portService = httpService.port();
+        String host = hostService + ":" + portService;
+        // Path
+        String path = requestResponse.request().path();
+        // Request Header
+        List<HttpHeader> headersRequest = requestResponse.request().headers();
+        String headersToString = headersRequest.stream()
+                .map(HttpHeader::toString)
+                .reduce((header1, header2) -> header1 + "\n" + header2)
+                .orElse(""); // use reduce to concatenate headers
+        // Response Header
+        List<HttpHeader> headersResponse = requestResponse.response().headers();
+        String headersResponseToString = headersResponse.stream()
+                .map(HttpHeader::toString)
+                .reduce((header1, header2) -> header1 + "\n" + header2)
+                .orElse(""); // use reduce to concatenate headers
+        String firstLine = requestResponse.response().toString().split("\n")[0];
+        headersResponseToString = firstLine + "\n" + headersResponseToString;
+        // Request Body
+        ByteArray requestBodyByte = requestResponse.request().toByteArray();
+
+        // Strip all control characters (0x00 - 0x1F) and non-ASCII characters (0x80+)
+        String requestBody = new String(ExcelFormatter.filterValidASCII(requestBodyByte.getBytes()), StandardCharsets.UTF_8);
+
+        // Response Body
+        String responseBody = requestResponse.response().bodyToString();
+        // Request response raw
+        String tmp = "______ REQUEST ______\n";
+        tmp += "GET Params\n";
+        List<ParsedHttpParameter> params = requestResponse.request().parameters(HttpParameterType.URL);
+        for (ParsedHttpParameter param : params) {
+            tmp += param.name() + " | ";
+        }
+        tmp += "\nPOST Params\n";
+        List<ParsedHttpParameter> paramsPost = requestResponse.request().parameters(HttpParameterType.BODY);
+        for (ParsedHttpParameter param : paramsPost) {
+            tmp += param.name() + " | ";
+        }
+        tmp += "\nHEADERS\n";
+        for (HttpHeader header : headersRequest) {
+            tmp += header.name() + " | ";
+        }
+        tmp += "\nCookie\n";
+        List<ParsedHttpParameter> cookies = requestResponse.request().parameters(HttpParameterType.COOKIE);
+        for (ParsedHttpParameter cookie : cookies) {
+            tmp += cookie.name() + " | ";
+        }
+
+        tmp += "\n\n______ RESPONSE ______\n";
+
+        // tmp += "\nPOST Params\n";
+        // List<ParsedHttpParameter> paramsPostResponse =
+        // requestResponse.response().parameters(HttpParameterType.BODY);
+        // for (ParsedHttpParameter param: paramsPostResponse) {
+        // tmp += param.name() + " | " ;
+        // }
+        tmp += "\nHEADERS\n";
+        for (HttpHeader header : headersResponse) {
+            tmp += header.name() + " | ";
+        }
+        tmp += "\nCookie\n";
+        List<Cookie> cookiesResponse = requestResponse.response().cookies();
+        for (Cookie cookie : cookiesResponse) {
+            tmp += cookie.name() + " | ";
+        }
+        tmp += "\n\n______ RAW ______\n";
+        // ------------------------------------------------
+        // method
+        excelFormatter.addData(ExcelFormatter.excelFormat(method));
+        // host
+        excelFormatter.addData(ExcelFormatter.excelFormat(host));
+        // path
+        excelFormatter.addData(ExcelFormatter.excelFormat(path));
+        // request header
+        excelFormatter.addData(ExcelFormatter.excelFormat(headersToString));
+        // response header
+        excelFormatter.addData(ExcelFormatter.excelFormat(headersResponseToString));
+        // request body
+        excelFormatter.addData(ExcelFormatter.excelFormat(requestBody));
+        // response body
+        excelFormatter.addData(ExcelFormatter.excelFormat(responseBody));
+        // request response raw
+        excelFormatter.addData(ExcelFormatter.excelFormat(tmp));
+        // ------------------------------------------------
+        // Copy to clipboard
+        String data = excelFormatter.getData();
+        StringSelection stringSelection = new StringSelection(data);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+    }
+
+    public void handleCopyToExcelNoBody(HttpRequestResponse requestResponse) {
+        ExcelFormatter excelFormatter = new ExcelFormatter();
+
+        // -----------------------------------------------
+        // Method
+        String method = requestResponse.request().method();
+        // Host
+        HttpService httpService = requestResponse.httpService();
+        String hostService = httpService.host();
+        int portService = httpService.port();
+        String host = hostService + ":" + portService;
+        // Path
+        String path = requestResponse.request().path();
+        // Request Header
+        List<HttpHeader> headersRequest = requestResponse.request().headers();
+        String headersToString = headersRequest.stream()
+                .map(HttpHeader::toString)
+                .reduce((header1, header2) -> header1 + "\n" + header2)
+                .orElse(""); // use reduce to concatenate headers
+        // Response Header
+        List<HttpHeader> headersResponse = requestResponse.response().headers();
+        String headersResponseToString = headersResponse.stream()
+                .map(HttpHeader::toString)
+                .reduce((header1, header2) -> header1 + "\n" + header2)
+                .orElse(""); // use reduce to concatenate headers
+        String firstLine = requestResponse.response().toString().split("\n")[0];
+        headersResponseToString = firstLine + "\n" + headersResponseToString;
+        // Request Body
+        String requestBody = requestResponse.request().bodyToString();
+        // Response Body
+        String responseBody = "<redacted>";
+        // Request response raw
+        String tmp = "______ REQUEST ______\n";
+        tmp += "GET Params\n";
+        List<ParsedHttpParameter> params = requestResponse.request().parameters(HttpParameterType.URL);
+        for (ParsedHttpParameter param : params) {
+            tmp += param.name() + " | ";
+        }
+        tmp += "\nPOST Params\n";
+        List<ParsedHttpParameter> paramsPost = requestResponse.request().parameters(HttpParameterType.BODY);
+        for (ParsedHttpParameter param : paramsPost) {
+            tmp += param.name() + " | ";
+        }
+        tmp += "\nHEADERS\n";
+        for (HttpHeader header : headersRequest) {
+            tmp += header.name() + " | ";
+        }
+        tmp += "\nCookie\n";
+        List<ParsedHttpParameter> cookies = requestResponse.request().parameters(HttpParameterType.COOKIE);
+        for (ParsedHttpParameter cookie : cookies) {
+            tmp += cookie.name() + " | ";
+        }
+
+        tmp += "\n\n______ RESPONSE ______\n";
+
+        // tmp += "\nPOST Params\n";
+        // List<ParsedHttpParameter> paramsPostResponse =
+        // requestResponse.response().parameters(HttpParameterType.BODY);
+        // for (ParsedHttpParameter param: paramsPostResponse) {
+        // tmp += param.name() + " | " ;
+        // }
+        tmp += "\nHEADERS\n";
+        for (HttpHeader header : headersResponse) {
+            tmp += header.name() + " | ";
+        }
+        tmp += "\nCookie\n";
+        List<Cookie> cookiesResponse = requestResponse.response().cookies();
+        for (Cookie cookie : cookiesResponse) {
+            tmp += cookie.name() + " | ";
+        }
+        tmp += "\n\n______ RAW ______\n";
+        // ------------------------------------------------
+        // method
+        excelFormatter.addData(ExcelFormatter.excelFormat(method));
+        // host
+        excelFormatter.addData(ExcelFormatter.excelFormat(host));
+        // path
+        excelFormatter.addData(ExcelFormatter.excelFormat(path));
+        // request header
+        excelFormatter.addData(ExcelFormatter.excelFormat(headersToString));
+        // response header
+        excelFormatter.addData(ExcelFormatter.excelFormat(headersResponseToString));
+        // request body
+        excelFormatter.addData(ExcelFormatter.excelFormat(requestBody));
+        // response body
+        excelFormatter.addData(ExcelFormatter.excelFormat(responseBody));
+        // request response raw
+        excelFormatter.addData(ExcelFormatter.excelFormat(tmp));
+        // ------------------------------------------------
+        // Copy to clipboard
+        String data = excelFormatter.getData();
+        StringSelection stringSelection = new StringSelection(data);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
     }
 }
